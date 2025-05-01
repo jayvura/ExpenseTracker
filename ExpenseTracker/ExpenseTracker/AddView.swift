cf6cb51dd568d1bb3c847fd8f1e5030f739a8de2
Jayden <jdecambre10@gmail.com>
2025-04-30 22:05:58 -0400

    finished expense tracker

---
+++ ExpenseTracker/ExpenseTracker/AddView.swift
@@ -0,0 +1,50 @@
+//
+//  AddView.swift
+//  ExpenseTracker
+//
+//  Created by Jayden on 4/28/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business", "Personal"]
+    
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
